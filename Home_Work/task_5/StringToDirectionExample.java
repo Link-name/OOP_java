1590e5b016a65ac515f45b4c9e69bc1a81c5f360
Link <stereo@mail.com>
2023-03-17 15:35:17 +0300

    DZ 5

--- Home_Work/task_5/StringToDirectionExample.java
+++ Home_Work/task_5/StringToDirectionExample.java
@@ -5,6 +5,6 @@ public class StringToDirectionExample {
     // public static void main(String[] args) {
     //     Direction direction = Direction.ofString("LEFT");
     //     System.out.println(direction);
-    // }
+    // 
 
 }
